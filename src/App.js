add6add5ea9959361a3817792b8dc69049442f48
Arun Subash Manickam <arunsubashm@gmail.com>
2021-01-30 15:53:16 +0530

    Displayed Phone Directory - 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
-function App() {
+class App extends Component {
   render() {
-  return (
-    <div>
+    return (
+      <div>
         Phone Directory
-    </div>
-  );
+      </div>
+    );
   }
 }
 
