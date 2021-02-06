0519d85d1c4509a77005bb35ce9bd4f5b4ca6ca1
Arun Subash Manickam <arunsubashm@gmail.com>
2021-02-06 18:29:15 +0530

    Styling - part 2

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
+import './Header.css';
 
 const Header = function () {
-  const headerStyle = {
-      textAlign: 'center',
-      padding: 20,
-      background: '#000',
-      color: '#fff',
-      textTransform: 'uppercase'
-  };
+  // const headerStyle = {
+  //     textAlign: 'center',
+  //     padding: 20,
+  //     background: '#000',
+  //     color: '#fff',
+  //     textTransform: 'uppercase'
+  // };
 
 
    return (
-       <div style={headerStyle}>
+       //<div style={headerStyle}>
+       <div className="header">
          Phone Directory
        </div>
    )
