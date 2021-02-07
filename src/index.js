a9a9e21fa912adf4124da0db1ea1db63a7647b3f
Arun Subash Manickam <arunsubashm@gmail.com>
2021-02-07 10:35:38 +0530

    Implmentation of State

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import App from './App';
 import AddSusbscriber from './AddSubscriber';
 import './common.css';
 
-ReactDOM.render(<AddSusbscriber />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
